92de315fc8948d73577ea0615af917ed3e7a03dd
Manoj <101315618+manojmarri@users.noreply.github.com>
2025-12-06 22:36:52 +0530

    changes

--- src/app/Modules/footer/footer.ts
+++ src/app/Modules/footer/footer.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-footer',
@@ -7,13 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './footer.scss',
 })
 export class Footer {
-  whatsappNumber: string = '9908460521';
+  whatsappNumber: string = '9948227227';
   defaultMessage: string = 'Hello';
-
+  toaster = inject(ToastrService)
 
   openWhatsAppChat(): void {
     const encodedMessage = encodeURIComponent(this.defaultMessage);
     const whatsappLink = `https://wa.me/${this.whatsappNumber}?text=${encodedMessage}`;
     window.open(whatsappLink, '_blank');
+  }
+
+  toast(){
+    this.toaster.warning('Available Soon')
   }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,21 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
+import { BrowserModule } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    BrowserModule,
+    provideAnimations(), 
+    provideToastr({
+      timeOut: 2000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+    })
   ]
 };
